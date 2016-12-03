92b8c8e76900bfe124d180ae5dcdbb5c7ae98cd2
Sheldon93 <S1364143@student.mcckc.edu>
2016-12-03 14:24:54 -0600

    Update actiongame.java

--- actiongame.java
+++ actiongame.java
@@ -10,28 +10,49 @@ public class actiongame
 	
 	// Variables
 	boolean win = false;
+	String name;
 	
+	// Run Game
+	public void runGame()
+	{
+		menuIntro();
+		board.printBoard();
+		board.placeShips();
+		playerGuess(); //Still in progress
+		// To Do
+	}
+	
+	// Get player name, just for fun.
+	public String getName()
+	{
+		System.out.print("ENTER YOUR FIRST OR LAST NAME, JEDI: ");
+		name = input.next();
+		return this.name;
+	}
 	
 	// Intro to the Game.
 	public void menuIntro()
 	{
+		String playerName;	
 		System.out.println("  **********************************");
 		System.out.println("  *      ********************      *");
 		System.out.println("  *      STAR WARS BATTLESHIP!     *");
 		System.out.println("  *      ********************      *");
 		System.out.println("  **********************************");
-		System.out.println("\nSelect your difficulty Level\n");
-		System.out.println("  **********************************");
-		System.out.println("1 - Jedi PADAWAN - 6x6 - 30 Missiles");
-		System.out.println("2 - Jedi KNIGHT  - 9x9 - 50 Missiles");
-		System.out.println("3 - Jedi MASTER - 12x12 - 75 Missiles");
-		System.out.println("  **********************************");
-		System.out.printf("\nChoose your difficulty (1-3): ");
+		playerName = getName().toUpperCase();
+		System.out.println("\nDIFFICULTY LEVELS TO CHOOSE FROM, YOUNG " + playerName + "\n");
+		System.out.println("************************************************");
+		System.out.println("* 1 - Jedi PADAWAN " + playerName.toUpperCase() + " - 6x6 - 30 Missiles *");
+		System.out.println("* 2 - Jedi KNIGHT " + playerName.toUpperCase() + " - 9x9 - 50 Missiles  *");
+		System.out.println("* 3 - Jedi MASTER " + playerName.toUpperCase() + " - 12x12 - 75 Missiles*");
+		System.out.println("************************************************");
+		System.out.printf("\nNow, YOUNG " + playerName + ", Enter your difficulty (1-3): ");
 	}
 	
-	// Player guess Ship locations
-	public guess()
+	//
+	
+	//player guess row
+	public void playerGuess()
 	{
-		//TO DO
+		// Still in progress.
 	}
-}
